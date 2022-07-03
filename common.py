83988703e903118443c12f4458bf3270e1f49d49
oldpilgrim <40671177+oldpilgrim@users.noreply.github.com>
2022-07-03 17:14:06 +0100

    add 04a identifying propaganda

--- common.py
+++ common.py
@@ -23,6 +23,28 @@ headers = {
 }
 
 
+img_headers = {
+    # ':authority': 'img9.doubanio.com',
+    # ':method': 'GET',
+    # ':path': '/view/photo/s_ratio_poster/public/p1524023814.jpg',
+    # ':scheme': 'https',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+    'cache-control': 'no-cache',
+    'pragma': 'no-cache',
+    'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+}
+
+
 def get_headers():
     return {
         # "spread operator"
